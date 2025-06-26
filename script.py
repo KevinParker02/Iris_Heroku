3d5cdf3b904f83c20cf87d1090f7964028dac0a3
KevinVivanco_02 <131885520+KevinParker02@users.noreply.github.com>
2025-06-25 21:30:32 -0400

    tatan

--- script.py
+++ script.py
@@ -1,47 +1,50 @@
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import numpy as np
 import pickle
 import os
 
-# Cargar templates
-templates = Jinja2Templates(directory="templates")
-
-# Crear app
 app = FastAPI()
 
-# Cargar modelo
-modelo = pickle.load(open("checkpoints/model.pkl", "rb"))
+# Configurar rutas de templates y archivos estáticos
+templates = Jinja2Templates(directory="templates")
 
+# Función de predicción
+def ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1, 4)
+    loaded_model = pickle.load(open("checkpoints/model.pkl", "rb"))
+    result = loaded_model.predict(to_predict)
+    return result[0]
+
+# Ruta index
 @app.get("/", response_class=HTMLResponse)
 @app.get("/index", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+# Ruta resultado
 @app.post("/result", response_class=HTMLResponse)
 async def result(
     request: Request,
-    feature1: float = Form(...),
-    feature2: float = Form(...),
-    feature3: float = Form(...),
-    feature4: float = Form(...)
+    sepal_length: float = Form(...),
+    sepal_width: float = Form(...),
+    petal_length: float = Form(...),
+    petal_width: float = Form(...)
 ):
+    to_predict_list = [sepal_length, sepal_width, petal_length, petal_width]
     try:
-        datos = [feature1, feature2, feature3, feature4]
-        pred = modelo.predict([np.array(datos)])[0]
-        if int(pred) == 0:
+        result = ValuePredictor(to_predict_list)
+        if int(result) == 0:
             prediction = 'Iris-Setosa'
-        elif int(pred) == 1:
+        elif int(result) == 1:
             prediction = 'Iris-Virginica'
-        elif int(pred) == 2:
+        elif int(result) == 2:
             prediction = 'Iris-Versicolour'
         else:
-            prediction = f'{int(pred)} No-definida'
-    except ValueError:
+            prediction = f'{int(result)} No-definida'
+    except:
         prediction = 'Error en el formato de los datos'
 
-    return templates.TemplateResponse("result.html", {
-        "request": request,
-        "prediction": prediction
-    })
+    return templates.TemplateResponse("result.html", {"request": request, "prediction": prediction})
